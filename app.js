55d96ba84c9c5dfa225f1fc97cc8730b45c62485
Muscardina <vergonzalezsainz@gmail.com>
2024-10-09 10:20:51 +0200

    Funcionando en local

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const connection = require('./config');
 const app = express();
-const PORT = process.env.APP_PORT;
+const PORT = 3000;
 
 // Servir archivos estáticos (HTML, CSS, JS)
 app.use(express.static(path.join(__dirname, 'public')));
--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 // config.js
 const mysql = require('mysql');
-require('dotenv').config();
+//require('dotenv').config();
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  host: 'localhost',
+  user: 'root',
+  password: 'krono',
+  database: 'school'
 });
 
 connection.connect((err) => {
